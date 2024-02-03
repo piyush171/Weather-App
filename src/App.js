61d4fec93a413464d5898e05ff68f2dc5fd9d6d7
piyush171 <70268673+piyush171@users.noreply.github.com>
2024-02-03 11:07:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import HomeComponents from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeComponents />
+    </div>
+  );
+}
+
+export default App;
+
